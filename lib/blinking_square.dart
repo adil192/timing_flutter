5d3a66d5daeb20f9b1ea8d659dca080ad98de619
Adil Hanney <adilhanney@disroot.org>
2025-11-12 19:10:40 +0000

    ref: use bool tween instead of haphazard int parsing

--- lib/blinking_square.dart
+++ lib/blinking_square.dart
@@ -19,62 +19,62 @@ class BlinkingSquare extends StatefulWidget {
 
 class _BlinkingSquareState extends State<BlinkingSquare>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late CurvedAnimation _animation;
+  late final _controller = AnimationController(vsync: this);
+  late var _tween = BlinkTween(blinkOnDuration: Duration.zero, easyMode: true);
 
-  late Duration _currentDuration;
-
-  bool _blinkOn = true;
-  double _lastAnimationValue = 0;
+  /// Animation that outputs true when the square should be visible.
+  late var _animation = _tween.animate(_controller);
 
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 0),
-      vsync: this,
-    );
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.linear);
-    resetAnimation();
-    _animation.addListener(() {
-      // don't setSate if not changed
-      if (_animation.value == _lastAnimationValue) return;
-      setState(() {
-        _lastAnimationValue = _animation.value;
-      });
-    });
+    _updateAnimation();
+    _animation.addListener(_onBlink);
     _controller.repeat(reverse: false);
   }
 
-  void resetAnimation() {
-    _currentDuration = widget.blinkOnDuration;
-    BlinkCurve curve = BlinkCurve(
+  @override
+  void dispose() {
+    _animation.removeListener(_onBlink);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant BlinkingSquare oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isBlinking != oldWidget.isBlinking ||
+        widget.blinkOnDuration != oldWidget.blinkOnDuration) {
+      _updateAnimation();
+    }
+  }
+
+  void _updateAnimation() {
+    _tween = BlinkTween(
       blinkOnDuration: widget.blinkOnDuration,
       easyMode: stows.easyMode.value,
     );
-    _animation.curve = curve;
-    _controller.duration = Duration(milliseconds: curve.durationMs);
+    _controller.duration = Duration(milliseconds: _tween.periodMs);
+    _animation.removeListener(_onBlink);
+    _animation = widget.isBlinking
+        ? _tween.animate(_controller)
+        : AlwaysStoppedAnimation(true);
+    _animation.addListener(_onBlink);
+  }
+
+  void _onBlink() {
+    if (mounted) setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.blinkOnDuration != _currentDuration) resetAnimation();
-
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
-
-    if (widget.isBlinking) {
-      _blinkOn = _animation.value == 0;
-    } else {
-      _blinkOn = true;
-    }
-
+    final colorScheme = ColorScheme.of(context);
     return AspectRatio(
       aspectRatio: 1,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(10)),
-          color: _blinkOn ? colorScheme.primary : Colors.transparent,
+          color: _animation.value ? colorScheme.primary : Colors.transparent,
         ),
         child: widget.child,
       ),
@@ -82,19 +82,19 @@ class _BlinkingSquareState extends State<BlinkingSquare>
   }
 }
 
-class BlinkCurve extends Curve {
-  late final int durationMs;
-
+class BlinkTween extends Tween<bool> {
+  late final int periodMs;
   late final double threshold; // 0 to 1
-  BlinkCurve({required Duration blinkOnDuration, required bool easyMode}) {
+  BlinkTween({required Duration blinkOnDuration, required bool easyMode})
+    : super(begin: true, end: false) {
     final int blinkOnMs = blinkOnDuration.inMilliseconds;
     final int blinkOffMs = easyMode ? 1000 : blinkOnDuration.inMilliseconds;
-    durationMs = blinkOnMs + blinkOffMs;
-    threshold = blinkOnMs / durationMs;
+    periodMs = blinkOnMs + blinkOffMs;
+    threshold = blinkOnMs / periodMs;
   }
 
   @override
-  double transform(double t) {
-    return t < threshold ? 0 : 1;
+  bool lerp(double t) {
+    return t < threshold ? begin! : end!;
   }
 }
